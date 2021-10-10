fccb404215ca566f3a80de2a069c2fc7f9621282
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-10-10 16:58:29 +0900

    Update test

--- test.ts
+++ test.ts
@@ -1,29 +1,58 @@
-import * as Matcher from "./denops/@ddc-filters/matcher_fuzzy.ts"
-import * as Sorter from "./denops/@ddc-filters/sorter_fuzzy.ts"
-import * as Converter from "./denops/@ddc-filters/converter_fuzzy.ts"
-import {assertEquals} from "https://lib.deno.dev/std@0.106.0/testing/asserts.ts"
+import * as Matcher from "./denops/@ddc-filters/matcher_fuzzy.ts";
+import * as Sorter from "./denops/@ddc-filters/sorter_fuzzy.ts";
+import * as Converter from "./denops/@ddc-filters/converter_fuzzy.ts";
+import { assertEquals } from "https://lib.deno.dev/std@0.106.0/testing/asserts.ts";
 
-const matcher = new Matcher.Filter()
-const sorter = new Sorter.Filter()
-const converter = new Converter.Filter()
+const matcher = new Matcher.Filter();
+const sorter = new Sorter.Filter();
+const converter = new Converter.Filter();
+
+function highlight(col: number) {
+  return {
+    type: "abbr",
+    name: "ddc_fuzzy_matched_character",
+    col,
+    width: 1,
+    "hl_group": "SpellBad",
+  };
+}
 
 Deno.test("converter", async () => {
   assertEquals(
-    await converter.filter({completeStr: "abc", candidates: [{word: '0a0b0c0'}, {word: 'abc'}, {word: 'xyz'}]}),
-    [{word:'0a0b0c0', abbr: '0(a)0(b)0(c)0'}, {word: 'abc', abbr: '(abc)'}, {word: 'xyz', abbr: 'xyz'}]
-  )
-})
+    await converter.filter({
+      sourceOptions: { ignoreCase: false },
+      completeStr: "abc",
+      candidates: [{ word: "0a0b0c0" }, { word: "abc" }, { word: "xyz" }],
+    } as any),
+    [
+      {
+        word: "0a0b0c0",
+        highlights: [highlight(1), highlight(3), highlight(5)],
+      },
+      { word: "abc", highlights: [highlight(0), highlight(1), highlight(2)] },
+      { word: "xyz", highlights: [] },
+    ],
+  );
+});
 
 Deno.test("matcher", async () => {
   assertEquals(
-    await matcher.filter({completeStr: "abc", candidates: [{word: '0a0b0c0'}, {word: 'abc'}, {word: 'xyz'}]}),
-    [{word:'0a0b0c0'}, {word: 'abc'}]
-  )
-})
+    await matcher.filter({
+      sourceOptions: { ignoreCase: false },
+      completeStr: "abc",
+      candidates: [{ word: "0a0b0c0" }, { word: "abc" }, { word: "xyz" }],
+    } as any),
+    [{ word: "0a0b0c0" }, { word: "abc" }],
+  );
+});
 
 Deno.test("sorter", async () => {
   assertEquals(
-    await sorter.filter({completeStr: "abc", candidates:[{word: '0a0b0c0'}, {word: 'abc'}]}),
-    [{word: 'abc'}, {word: '0a0b0c0'}]
-  )
-})
+    await sorter.filter({
+      sourceOptions: { ignoreCase: false },
+      completeStr: "abc",
+      candidates: [{ word: "0a0b0c0" }, { word: "abc" }],
+    } as any),
+    [{ word: "abc" }, { word: "0a0b0c0" }],
+  );
+});
